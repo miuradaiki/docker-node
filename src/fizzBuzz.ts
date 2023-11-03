3c3f1cc71d4db562b19f557210b628a2d1d4f299
Daiki <daiki.miura@prum.jp>
2023-11-03 17:37:53 +0900

    add: fizzBuzz

--- src/fizzBuzz.ts
+++ src/fizzBuzz.ts
@@ -1,16 +1,42 @@
-let result = "";
+function getFizzBuzzString(i: number): string {
+    if (i % 3 === 0 && i % 5 === 0) {
+      return "FizzBuzz";
+    } else if (i % 3 === 0) {
+      return "Fizz";
+    } else if (i % 5 === 0) {
+      return "Fizz";
+    } else {
+      return String(i);
+    }
+}
+
 for (let i =1; i <= 100; i++) {
-  if (i > 1) {
-    result += " ";
-  };
-  if (i % 3 === 0 && i % 5 === 0) {
-    result += "FizzBuzz";
-  } else if (i % 3 === 0) {
-    result += "Fizz";
-  } else if (i % 5 === 0) {
-    result += "Buzz";
-  } else {
-    result += String(i);
+  const message = getFizzBuzzString(i);
+  console.log(message);
+}
+
+function sequence(start: number, end: number): number[] {
+  const result: number[] = [];
+  for (let i = start; i < end; i++) {
+    result.push(i);
   }
-};
+  return result
+}
+
+for (const i of sequence(1, 100)) {
+  const message = getFizzBuzzString(i);
+  console.log(message);
+}
+
+// コールバック関数の練習
+function map<T, U>(array: T[], callback: (value: T) => U): U[] {
+  const result: U[] = [];
+  for (const element of array) {
+    result.push(callback(element));
+  }
+  return result
+}
+
+const data = [1, 1, 2, 3, 5, 8, 13];
+const result = map(data, (x) => x * 10);
 console.log(result);
